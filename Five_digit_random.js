1545bb3ddb7f1bc4ca4a6c34542efc7fe2e2bfef
GENIUS <naikmahesh.naik@gmail.com>
2021-04-06 22:23:53 +0530

    Add five digit random values adn get Addition of five random value and its average

---
+++ Five_digit_random.js
@@ -0,0 +1,17 @@
+var R1 = Math.floor(Math.random()*100);
+var R2 = Math.floor(Math.random()*100);
+var R3 = Math.floor(Math.random()*100);
+var R4 = Math.floor(Math.random()*100);
+var R5 = Math.floor(Math.random()*100);
+
+console.log("First_Number:" +R1);
+console.log("Second_Number:" +R2);
+console.log("Third_Number:" +R3);
+console.log("Forth_Number:" +R4);
+console.log("Fifth_Number:" +R5);
+
+var sum =  R1+R2+R3+R4+R5;
+console.log("SUM:" +sum);
+
+var Avg = sum/5;
+console.log("Avg:" +Avg);
