ffabb10842da5dcb6dbadd4d87a22c2b5baf5173
GENIUS <naikmahesh.naik@gmail.com>
2021-04-07 20:11:20 +0530

    Selection With If Else Practise Problems modify UC2

--- ifelse/day_month.js
+++ ifelse/day_month.js
@@ -1,17 +1,28 @@
-
-let result = false;
-
-if(month == 'March') {
-    if (day > 20) {
-        result = true;
+var month, day;
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+readline.question(`Enter the Month `, value => {
+    console.log(`The Month is ${value}`)
+    month=value;
+    readline.question(`Enter the Day `, values => {
+        console.log(`The Day is ${values}`)
+        day=values;
+        checkDate(month, day)
+        readline.close();
+    })
+})
+function checkDate(month, day) {
+    if ((month >= 3) && (month <= 6)) {
+        var maxdaysinmonth = 30 + (month % 2);
+        if ((day > 20) && (day <= maxdaysinmonth)) {
+            console.log("True : " +day +"-"+ month )
+        }else {
+            console.log("False : " +day +"-"+ month )
+        }
+    }
+    else {
+        console.log("False : " +day +"-"+ month )
     }
 }
-if (month == 'April' || month == 'May') {
-   result = true;
-}
-if (month == 'June') {
-    if (day < 20) {
-        result = true;
-    }
-}
-console.log("Is day and month between given period: " + result);
